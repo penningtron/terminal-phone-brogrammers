b1a4332d0f4a6782b9bc1506fe016ee633487bd3
nathan pennington <nathantp@gmail.com>
2024-02-02 16:54:47 -0600

    final working

--- phone.py
+++ phone.py
@@ -1,6 +1,7 @@
 from rich.console import Console
 from rich.table import Table
 from to_do_list import to_do_list
+from internet_search import open_browser
 
 
 def phone():
@@ -9,15 +10,15 @@ def phone():
     table.add_column("Options", style="cyan", no_wrap=True)
     table.add_column("Navigate", style="magenta")
     table.add_column("Icon", justify="center", style="green")
-    
+
     table.add_row("1", "Home", "🏠")
     table.add_row("2", "Contacts", "👽")
     table.add_row("3", "Internet", "🛜")
     table.add_row("4", "Tasks", "📝")
     table.add_row("5", "Exit", "❌")
-    
+
     console = Console()
-    while True:   
+    while True:
         console.print(table)
         print("Welcome to your Phone, please select an option from the menu below!")
         userInput = input("Choose one of the Options :")
@@ -29,6 +30,7 @@ def phone():
                 # import contacts.py
             case "3":
                 print("Opening a web browser")
+                open_browser()
                 # import internet_search.py
             case "4":
                 print("Opening To Do List")
@@ -39,27 +41,16 @@ def phone():
             case "5":
                 print("Goodbye")
                 break
-                
+
             case _:
                 print("Not a valid input, please select from the listed options.")
                 print(f"You entered: {userInput}")
-          
-   
 
-
-
-
-    
-    
-
-
-    
-   
  # Give the user options, number the options( 1 = home 2 = phone 3 = date)
-    
-    
-#The following are the menu options the assignment has asked us to implement; 
-    
-#1: Personalize the phone with images and colors.
-#2: Design a menu interface using basic Python functions.
+
+
+# The following are the menu options the assignment has asked us to implement;
+
+# 1: Personalize the phone with images and colors.
+# 2: Design a menu interface using basic Python functions.
 phone()
