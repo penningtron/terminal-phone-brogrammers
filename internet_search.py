1560259956b554ef940aa97393a101250a919346
Tom Childress <thomas.childress02@gmail.com>
2024-02-02 17:09:18 -0500

    condensed to one open_browser function for search functionality and history printing functionality

--- internet_search.py
+++ internet_search.py
@@ -1,4 +1,3 @@
-from googlesearch import search
 from webbrowser import open
 
 search_history = []
@@ -6,46 +5,26 @@ search_history = []
 # Helper function to get user input and append to search history
 def user_query():
     user_query_input = input("What would you like to search? ")
-    search_history.append(user_query_input)
     return user_query_input
 
 # Function to open a web browser
 def open_browser():
-    open(url = "https://www.google.com", new = 2)
-    
-    
-# Search function
-def google_search():
-    query = user_query()
-    resultList = list(search(query, num = 5, stop = 5))
-    for elem in resultList:
-        print(elem)
-    choice = input("Would you like to make another query (Y/N)? ")
-    if choice == "Y":
-        google_search()
-    elif choice == "N":
-        history_choice = input("Would you like to see your search history (Y/N)? ")
-        if history_choice == "Y":
-            for elem2 in search_history:
-                print(elem2)
-        elif history_choice == "N":
-            print("Thank you for searching.")
-        else:
-            raise ValueError("Not a valid choice.")
-    else:
+    user_choice = input("Would you like to search Google (Y/N)? ")
+    if user_choice != "Y" and user_choice != "N":
         raise ValueError("Not a valid choice.")
-    
-
-# def google_search2():
-#     user_choice = input("Would you like to search something (Y/N)? ")
-#     if user_choice != "Y" and user_choice != "N":
-#         raise ValueError("Not a valid choice.")
-#     elif user_choice == "Y":
-#         while user_choice == "Y":
-#             query = user_query
-#             resultList = list(search(query, num = 5, stop = 5))
-#             for elem in resultList:
-#                 print(elem)
-
-# google_search()
-# open_browser()
+    while user_choice == "Y":
+        query_choice = user_query()
+        search_history.append(query_choice)
+        open(url = f"https://www.google.com/search?q={query_choice}", new = 2)
+        user_choice = input("Would you like to search again (Y/N)? ")
+        if user_choice != "Y" and user_choice != "N":
+            raise ValueError("Not a valid choice.")
+    if user_choice == "N":
+        history_query = input("Would you like to print your search history (Y/N)? ")
+        if history_query != "Y" and history_query != "N":
+            raise ValueError("Not a valid choice.")
+        elif history_query == "Y":
+            for elem in search_history:
+                print(elem)
+        elif history_query == "N":
+            pass
